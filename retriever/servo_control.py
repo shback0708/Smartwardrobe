af89973c37ecd59038c4381aacabccbeeae4537c
Daniel Back <dback@Daniels-Air.fios-router.home>
2021-04-11 14:31:31 -0400

    successfully used flask to display index.htm

--- retriever/servo_control.py
+++ retriever/servo_control.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python3
-# from pyfirmata import Arduino, util
 import time
 import serial
 import retriever as ret
 
-serialcomm = serial.Serial('/dev/cu.usbmodem101', 9600)
-serialcomm.timeout = 1
+def rotate_servo(angle):
+    serialcomm = serial.Serial('/dev/cu.usbmodem101', 9600)
+    serialcomm.timeout = 1
+    angle_to_string = str(angle) + "\n"
+    serialcomm.write(angle_to_string.strip().encode())
+    serialcomm.close()
 
-while True:
-    #angle = ret.getAngle()
-    for i in range (0, 180, 20):
-        #convert int to string
-        angle = str(i) + "\n"
-        serialcomm.write(angle.strip().encode())
-        time.sleep(2)
-    # if angle == "done":
-    #     print("finished program")
-    #     break
-    # serialcomm.write(angle)
+rotate_servo(180)
+
+# we don't need a while true loop
+# if we just want to test the servo we just uncomment the part below
+# and comment the part above
+
+# serialcomm = serial.Serial('/dev/cu.usbmodem101', 9600)
+# serialcomm.timeout = 1
+# while True:
+#     #angle = ret.getAngle()
+#     for i in range (0, 180, 20):
+#         #convert int to string
+#         angle = str(i) + "\n"
+#         serialcomm.write(angle.strip().encode())
+#         time.sleep(2)
+#     # if angle == "done":
+#     #     print("finished program")
+#     #     break
+#     # serialcomm.write(angle)
     
-serialcomm.close()
+# serialcomm.close()
--- user_interface/user_interface.py
+++ user_interface/user_interface.py
@@ -12,26 +12,26 @@ def add():
     print("going to add.htm")
     return render_template("add.htm")
 
-@app.route("/add/clothes")
-def add_clothes():
-    # Actually here, user can input pictures and we can just call the visualizer
-    # api with that picture 
-    # they will have bunch of buttons they can select from
-    # look up the database for empty location (id = #)
-    # add to the emtpy location
-    userRequest(id)
+# @app.route("/add/clothes")
+# def add_clothes():
+#     # Actually here, user can input pictures and we can just call the visualizer
+#     # api with that picture 
+#     # they will have bunch of buttons they can select from
+#     # look up the database for empty location (id = #)
+#     # add to the emtpy location
+#     userRequest(id)
 
 
 @app.route("/remove")
-def add():
+def remove():
     return render_template("remove.htm")
 
-@app.route("/return")
-def add():
-    return render_template("return.htm")
+@app.route("/ret")
+def ret():
+    return render_template("ret.htm")
 
 @app.route("/take")
-def add():
+def take():
     return render_template("take.htm")
 
 if __name__ == "__main__":
