2002bafa41a50cdf224b7ecd08767bcd4f3d492a
Daniel Back <dback@Daniels-Air.fios-router.home>
2021-04-12 03:44:21 -0400

    ready for the demo

---
+++ retriever/test_servo/servo_control.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+Servo servo_test; // initialize servo object
+int angle = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  servo_test.attach(9); // pin 9 connected to servo
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (angle = 0; angle <= 180; angle += 20) {
+    servo_test.write(angle);
+    delay(2000);
+  }
+//  angle = 180;
+//  servo_test.write(angle);
+  //delay(5000);
+
+//  angle = 20;
+//  servo_test.write(angle);
+//  for (angle = 0; angle <= 180; angle += 20) {
+//    servo_test.write(angle);
+//    delay(1000);
+//  }
+  //delay(5000);
+
+}
