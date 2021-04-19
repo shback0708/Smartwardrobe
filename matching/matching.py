a41eaf5208917070e81aaca9d3c7de452a5ab0ec
Daniel Back <dback@Daniels-Air.fios-router.home>
2021-04-19 15:55:17 -0400

    added some comments

--- matching/matching.py
+++ matching/matching.py
@@ -1,8 +1,19 @@
 # This will be the stub file for matching api
 
+
+# we get the categories of clothes, and the colors of clothes
+# category and clothes will be lists
+
 # user inputed specs like jeans-only or black top
-def setFilter(a:string) -> int: ...
+# This function will call the database and return all the combinations
+
+def setFilter(category, color):
 
 # returns images of the best outfits 
-def getMatches() -> None: ...
+# output_of_setFilter will be a 2D array
+# output of getMatches will be all the images displayed to the user
+def getMatches(output_of_setFilter):
+
+
+
 
