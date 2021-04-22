5835df753f75b6a42c8137304fa0e640bd84a7f8
Daniel Back <dback@Daniels-Air.fios-router.home>
2021-04-22 03:05:08 -0400

    got the navigation bar working

--- user_interface/user_interface.py
+++ user_interface/user_interface.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_nav import Nav
+from flask_nav.elements import Navbar, Subgroup, View, Link, Text, Separator
 
 app = Flask(__name__)
+nav = Nav(app)
+
+nav.register_element('my_navbar', Navbar('thenav', 
+View('Home', 'index'), 
+View('Add', 'add'), 
+View('Remove', 'remove'),
+View('Return', 'ret'),
+View('Take', 'take')
+))
 
 @app.route("/")
 def index():
