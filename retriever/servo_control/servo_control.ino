d2811d85c0ef3ee0b8f66087ed0daa3102ad2bed
Daniel Back <dback@Daniels-Air.fios-router.home>
2021-04-27 15:11:36 -0400

    editted html file

--- retriever/servo_control/servo_control.ino
+++ retriever/servo_control/servo_control.ino
@@ -5,14 +5,23 @@ int angle = 0;
 void setup() {
   // put your setup code here, to run once:
   servo_test.attach(9); // pin 9 connected to servo
+  servo_test.write(0);
 }
 
 void loop() {
+  delay(30000);
   // put your main code here, to run repeatedly:
-  for (angle = 0; angle <= 180; angle += 20) {
+  for (angle = 0; angle <= 180; angle += 3) {
     servo_test.write(angle);
-    delay(2000);
+    delay(200);
   }
+
+  for (angle = 180; angle >= 0; angle -= 3) {
+    servo_test.write(angle);
+    delay(200);
+  }
+
+  delay (5000);
 //  angle = 180;
 //  servo_test.write(angle);
   //delay(5000);
