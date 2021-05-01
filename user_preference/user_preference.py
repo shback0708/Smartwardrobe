d8cbb2dff960b956be175286c7e68a8e32fac014
Yoo Joon Lee <yoojoonlee@gmail.com>
2021-04-30 21:34:38 -0400

    Update user_preference.py
    Changed user preference to take input of clothing objects

--- user_preference/user_preference.py
+++ user_preference/user_preference.py
@@ -1,5 +1,6 @@
 import os
-def setRating(p, outfit):
+import webcolors
+def setRating(p, inp):
   if not os.path.exists("fb.txt"):
     f = open("fb.txt", "w")
     f.close()
@@ -8,10 +9,16 @@ def setRating(p, outfit):
   lines = c.split("\n")
   like = ""
   dislike = ""
+  outfit = ""
   if(len(lines) >= 1):
     like = lines[0]
   if(len(lines) >= 2):
     dislike = lines[1]
+  if(len(inp) == 2):
+    outfit = inp[0][0] + " " + get_colour_name(inp[0][5]) + " "
+    outfit += inp[1][0] + " " + get_colour_name(inp[1][5])
+  else:
+    outfit =  inp[0] + " " + get_colour_name(inp[5])
   if(p == 1):
     outfits = like.split(",")
     if outfit not in outfits:
@@ -44,7 +51,7 @@ def setRating(p, outfit):
   f.write("\n")
   f.write(dislike)
   f.close()
-def getRating(outfit):
+def getRating(inp):
   try:
     f = open("fb.txt", "r")
   except:
@@ -52,6 +59,13 @@ def getRating(outfit):
   c = f.read()
   lines = c.split("\n")
   f.close()
+  outfit = ""
+  if(len(inp) == 2):
+    outfit = inp[0][0] + " " + get_colour_name(inp[0][5]) + " "
+    outfit += inp[1][0] + " " + get_colour_name(inp[1][5])
+  else:
+    outfit =  inp[0] + " " + get_colour_name(inp[5])
+
   if(len(lines) >= 1):
     good = lines[0].split(",")
     if outfit in good:
@@ -61,3 +75,26 @@ def getRating(outfit):
     if outfit in bad:
       return -1
   return 0
+def get_colour_name(rgb_triplet):
+    min_colours = {}
+    for key, name in webcolors.css21_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - rgb_triplet[0]) ** 2
+        gd = (g_c - rgb_triplet[1]) ** 2
+        bd = (b_c - rgb_triplet[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+if __name__ == '__main__':
+    a = (("shirt","a","b","c","d",(0,0,0)), ("pants","a","b","c","d",(0,0,0)))
+    b = (("dress","a","b","c","d",(100,0,0)))
+    c = (("shirt","a","b","c","d",(200,0,0)), ("pants","a","b","c","d",(100,0,0)))
+    d = c = (("shirt","a","b","c","d",(300,0,0)), ("pants","a","b","c","d",(200,0,0)))
+    setRating(1, a)
+    setRating(-1,b)
+    setRating(1, c)
+    setRating(1,d)
+    print(getRating(a))
+    print(getRating(b))
+    print(getRating(c))
+    print(getRating(d))
